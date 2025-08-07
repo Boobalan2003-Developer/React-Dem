ec94bec0a9dd7829ddfa3fa274461b0574f32e8e
Alan <boobalanvellingiri@gmail.com>
2025-08-07 11:42:26 +0530

    update commit

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -3,7 +3,7 @@ import '../styles/hero.css';
 function hero() {
     return (
         <div className="w-full md:w-1/2 lg:w-1/3 bg-red-500">
-            Resizes on screen size
+            <h1>Hello World...!</h1>
         </div>
     );
 }
