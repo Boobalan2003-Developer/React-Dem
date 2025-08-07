a6bb5d6f3f2afac51cd1a547bf2d30afc8e0f374
Alan <boobalanvellingiri@gmail.com>
2025-08-07 11:38:42 +0530

    Initial commit

---
+++ src/sections/hero.jsx
@@ -0,0 +1,11 @@
+import '../styles/hero.css';
+
+function hero() {
+    return (
+        <div className="w-full md:w-1/2 lg:w-1/3 bg-red-500">
+            Resizes on screen size
+        </div>
+    );
+}
+
+export default hero;
